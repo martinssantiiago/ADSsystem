d901704b02a8da96274bd621738b246f3dc6e83d
andre luiz martins santiago <santiiagoo2405@gmail.com>
2026-02-26 19:31:11 -0300

    botao de ver senha
    see password buttton

--- static/script.js
+++ static/script.js
@@ -53,4 +53,12 @@ document.addEventListener("DOMContentLoaded", () => {
     if (chatBox) carregarMensagens();
 });
 
+function ver() {
+    const input = document.querySelector("#isenha")
 
+    if(input.type === 'password') {
+        input.type = 'text';
+    } else {
+        input.type = 'password';
+    }
+}
